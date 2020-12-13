8bd83e99acbce2f459f3f285f8e1efe7d27734d0
amin rezazade <amin_r359@yahoo.com>
2020-12-13 18:54:29 +0330

    adding recipe, notes model entity

---
+++ src/main/java/com/spring5/recipe/domain/Notes.java
@@ -0,0 +1,40 @@
+package com.spring5.recipe.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNote;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNote() {
+        return recipeNote;
+    }
+
+    public void setRecipeNote(String recipeNote) {
+        this.recipeNote = recipeNote;
+    }
+}
