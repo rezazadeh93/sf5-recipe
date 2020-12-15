2439208fb5131888fb7dc15b837369833ea21b18
amin rezazade <amin_r359@yahoo.com>
2020-12-15 19:06:08 +0330

    adding ingredient model entity

--- src/main/java/com/spring5/recipe/domain/Recipe.java
+++ src/main/java/com/spring5/recipe/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.spring5.recipe.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -10,6 +11,9 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     private String description;
     private Integer prepTime;
@@ -24,6 +28,14 @@ public class Recipe {
 
     @Lob
     private Byte[] image;
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
 
     public Long getId() {
         return id;
