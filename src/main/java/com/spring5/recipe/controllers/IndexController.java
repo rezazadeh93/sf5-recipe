98bd9cdf56a2256b9e2aae6ef8c5db36cecdf9f0
amin rezazade <amin_r359@yahoo.com>
2020-12-16 17:36:26 +0330

    added jpa findBy method in repositories

--- src/main/java/com/spring5/recipe/controllers/IndexController.java
+++ src/main/java/com/spring5/recipe/controllers/IndexController.java
@@ -1,13 +1,32 @@
 package com.spring5.recipe.controllers;
 
+import com.spring5.recipe.domain.Category;
+import com.spring5.recipe.domain.UnitOfMeasure;
+import com.spring5.recipe.repositories.CategoryRepository;
+import com.spring5.recipe.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexController {
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
 
     @RequestMapping({"", "/", "/index"})
     public String getIndex() {
+        Optional<Category> optionalCategory = categoryRepository.findByDescription("Mexican");
+        Optional<UnitOfMeasure> optionalUnitOfMeasure = unitOfMeasureRepository.findByDescription("Tablespoon");
+
+        System.out.println(optionalCategory.get().getId());
+        System.out.println(optionalUnitOfMeasure.get().getId());
+
         return "index";
     }
 }
--- src/main/java/com/spring5/recipe/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/spring5/recipe/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package com.spring5.recipe.repositories;
 import com.spring5.recipe.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
