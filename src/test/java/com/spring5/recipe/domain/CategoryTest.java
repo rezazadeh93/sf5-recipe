5145441743aba6d39bab2b84d7354bb20791db1c
amin rezazade <amin_r359@yahoo.com>
2020-12-25 00:46:48 +0330

    added categoryTest file to tests

---
+++ src/test/java/com/spring5/recipe/domain/CategoryTest.java
@@ -0,0 +1,25 @@
+package com.spring5.recipe.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getRecipes() {
+    }
+}
