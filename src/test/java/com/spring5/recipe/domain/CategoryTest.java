5b782e88c8868e5061958a5b7df4a385807fcd03
Amin <amin.rezazadeh93@gmail.com>
2020-12-25 17:06:03 +0330

    added method tests to categoryTest

--- src/test/java/com/spring5/recipe/domain/CategoryTest.java
+++ src/test/java/com/spring5/recipe/domain/CategoryTest.java
@@ -6,20 +6,31 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CategoryTest {
+    Category category;
 
     @BeforeEach
     void setUp() {
+        category = new Category();
     }
 
     @Test
     void getId() {
+        Long valueId = 4L;
+
+        category.setId(valueId);
+        assertEquals(valueId, category.getId());
     }
 
     @Test
     void getDescription() {
+        String descTest = "This Description is for test!";
+
+        category.setDescription(descTest);
+        assertEquals(descTest, category.getDescription());
     }
 
     @Test
     void getRecipes() {
+        
     }
 }
