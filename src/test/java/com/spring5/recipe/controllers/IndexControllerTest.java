f3a6508256df77e21fcd0e0cc5a64f5ce45f63d4
amin rezazade <amin_r359@yahoo.com>
2020-12-27 12:04:38 +0330

    adding mvc mock test to indexController test

--- src/test/java/com/spring5/recipe/controllers/IndexControllerTest.java
+++ src/test/java/com/spring5/recipe/controllers/IndexControllerTest.java
@@ -1,21 +1,24 @@
 package com.spring5.recipe.controllers;
 
 import com.spring5.recipe.domain.Recipe;
-import com.spring5.recipe.repositories.RecipeRepository;
 import com.spring5.recipe.service.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class IndexControllerTest {
     IndexController indexController;
@@ -31,6 +34,15 @@ class IndexControllerTest {
         MockitoAnnotations.openMocks(this);
 
         indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 
     @Test
